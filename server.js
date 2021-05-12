e73f182f317115760c825c3dc803c2ac8bb7848a
Alex Ashley <alexa@liatrio.com>
2021-05-12 10:37:53 -0500

    Configure graceful shutdown (#29)

--- server.js
+++ server.js
@@ -2,15 +2,30 @@
 
 const express = require('express');
 
-// Constants
 const PORT = 8081;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    res.send('Hello World');
 });
 
-app.listen(PORT, HOST);
+const server = app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+const signalHandler = (signal) => () => {
+  console.log(`Received ${signal}, stopping server`)
+  server.close((error) => {
+    let exitCode = 0;
+    if (error) {
+      console.error('Error occurred stopping server', error);
+      exitCode = 1;
+    }
+
+    process.exit(exitCode)
+  });
+};
+
+['SIGINT', 'SIGTERM'].forEach((signal) => {
+    process.on(signal, signalHandler(signal));
+});
